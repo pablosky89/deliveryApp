a221aa92f0a2cc7e159e32e83e537d68511840f8
Pablo Sanhueza <pablo.sanhueza.fontalba@gmail.com>
2023-02-21 16:44:23 -0300

    project created

---
+++ backend/src/main/java/com/sanhueza/pablo/DeliverApp/DeliverAppApplication.java
@@ -0,0 +1,13 @@
+package com.sanhueza.pablo.DeliverApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sanhueza/pablo/DeliverApp/DeliverAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanhueza.pablo.DeliverApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
